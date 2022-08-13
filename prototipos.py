a1fda871dfc330db49ff70eff09ab1cd2f877253
Carlos Henrique Miranda Esteves <101829419+10CarlosEsteves@users.noreply.github.com>
2022-08-13 16:51:11 -0300

    Update prototipos.py

--- prototipos.py
+++ prototipos.py
@@ -6,7 +6,11 @@
 #porta = re.split(">[(A-Z)+_(0-9)+]",file)
 
 import re
-arquivo = open("virus.FASTA", "r")
+import os
+os.system("clear")
+nome = input("Por favor insira o nome do arquivo que deseja abrir:")
+
+arquivo = open(nome, "r")
 file = arquivo.read()
 
 chave = re.findall(">[A-Z]+_[0-9]+",file)
@@ -18,11 +22,13 @@ for i in range(0,5):
 
     biblioteca[chave[i]] = porta[i+1]
 
-X = input("Por favor insira a chave: ")
+comando = int(1)
 
-print(biblioteca[X])
+while(comando):
+    os.system("clear")
+    X = input("Por favor insira a chave para acessar o conteudo: ")
 
+    print(biblioteca[X])
+    comando=int(input("\nDeseja rodar o programa novamente?\n [1]SIM [NAO]\n>"))
 
-
-
-
+print("Programa encerrado!")
